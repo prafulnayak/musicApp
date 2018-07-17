eb58ebf9d18cd3f140405f6e3ed90a44389e5d7e
Praful Nayak <prafulnayak1988@gmail.com>
2018-07-17 06:40:58 +0530

    GridView on click item, set the image and song title in slide panel

--- app/src/main/java/org/sairaa/musicapp/SongFragment.java
+++ app/src/main/java/org/sairaa/musicapp/SongFragment.java
@@ -9,10 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class SongFragment extends Fragment {
+
+    private TextView textSlide;
+    private ImageView imageSlide, playImage, pauseImage;
 
     public SongFragment(){
 
@@ -23,7 +28,10 @@ public class SongFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.song_activity, container, false);
         final ArrayList<Music> songsToDisplay = new ArrayList<Music>();
-
+        textSlide = (TextView)getActivity().findViewById(R.id.text_slide);
+        imageSlide = getActivity().findViewById(R.id.image_slide);
+        playImage = getActivity().findViewById(R.id.play_image);
+        pauseImage = getActivity().findViewById(R.id.pause_image);
         songsToDisplay.add(new Music(getString(R.string.soorma),getString(R.string.soorma_song1),getString(R.string.sukhwinder),R.drawable.play_circle,R.drawable.soorma));
         songsToDisplay.add(new Music(getString(R.string.soorma),getString(R.string.soorma_song2),getString(R.string.shankarMahadevan),R.drawable.play_circle,R.drawable.soorma));
         songsToDisplay.add(new Music(getString(R.string.race3),getString(R.string.race3_song1),getString(R.string.meet_bros),R.drawable.play_circle,R.drawable.race3));
@@ -50,7 +58,10 @@ public class SongFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 Music sMusic = songsToDisplay.get(position);
-
+                imageSlide.setBackgroundResource(sMusic.getMusicResource());
+                textSlide.setText(sMusic.getSongName());
+                playImage.setVisibility(View.INVISIBLE);
+                pauseImage.setVisibility(View.VISIBLE);
             }
         });
 
